096478204acd6d3434a78ce6ce4b36bda116e41c
yonguk <wldyddnr@gmail.com>
2023-09-08 01:02:34 +0900

    공통 인터페이스 적용

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,7 +1,9 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+//@Repository 생략가능
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,6 +31,39 @@ class MemberRepositoryTest {
         assertThat(findMember.get().getId()).isEqualTo(member.getId());
         assertThat(findMember.get().getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember.get()).isEqualTo(member);
+
+    }
+
+    @Test
+    public void basicCRUD() throws Exception {
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //단건 조회
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(member1).isEqualTo(findMember1);
+        assertThat(member2).isEqualTo(findMember2);
+
+        //리스트 조회
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트
+        Long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        Long deleteCount = memberRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
 
     }
 
