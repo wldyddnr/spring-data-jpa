b18af5035517b983dda4f3abef87d33077677075
yonguk <wldyddnr@gmail.com>
2023-09-11 23:33:02 +0900

    @Query, 리포지토리 메소드에 쿼리 정의하기

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -103,5 +103,25 @@ class MemberRepositoryTest {
 
     }
 
+    @Test
+    public void testQuery() throws Exception {
+        //given
+
+        //when
+
+        //then
+
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        assertThat(result.size()).isEqualTo(1);
+
+
+    }
+
 
 }
