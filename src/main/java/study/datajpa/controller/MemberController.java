f3ca8a90e949764fd441bf7efed077c408f5ced9
yonguk <wldyddnr@gmail.com>
2023-09-19 19:03:49 +0900

    Web 확장 - 도메인 클래스 컨버터

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,35 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    //조회용으로만 사용 추천
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id){
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember(@PathVariable("id") Member member){
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("user"));
+    }
+
+}
